3dd93e7c943074537b8d7eab12192dc6cb22d47b
suryapsl <surya.nemiwal@purplestylelabs.com>
2025-04-04 16:32:30 +0530

    allow stage origin in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ["ui4.z1.stage.ppustage.dev"],
+  },
 })
